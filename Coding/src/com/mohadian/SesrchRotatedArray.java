3bc4b1b62e847597cbf94d50aa838c73bacb7338
Mostafa Hadian <mostafa.hadiandehkordi@sky.uk>
2019-05-09 22:34:03 +0100

    update binary search on rotated array

--- Coding/src/com/mohadian/SesrchRotatedArray.java
+++ Coding/src/com/mohadian/SesrchRotatedArray.java
@@ -2,32 +2,50 @@ package com.mohadian;
 
 public class SesrchRotatedArray {
 
-    public int findSequential(int[] a, int n) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == n) {
-                return i;
+    public int findBinaryRecursive(int[] nums, int target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[low] <= nums[mid]) {
+            if (target >= nums[low] && target <= nums[mid]) {
+                return findBinaryRecursive(nums, target, low, mid - 1);
+            } else {
+                return findBinaryRecursive(nums, target, mid + 1, high);
+            }
+        } else {
+            if (target >= nums[mid] && target <= nums[high]) {
+                return findBinaryRecursive(nums, target, mid + 1, high);
+            } else {
+                return findBinaryRecursive(nums, target, low, mid - 1);
             }
         }
-        return -1;
+
     }
 
-    public int findBinaryIterative(int[] a, int n) {
+    int findBinaryIterative(int[] nums, int target) {
         int low = 0;
-        int high = a.length - 1;
+        int high = nums.length - 1;
 
         while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (a[mid] == n) {
+            int mid = (low + high) / 2;
+
+            if (target == nums[mid]) {
                 return mid;
             } else {
-                if (a[low] < a[mid]) {
-                    if (a[mid] > n) {
+                if (nums[low] <= nums[mid]) {
+                    if (target >= nums[low] && target <= nums[mid]) {
                         high = mid - 1;
                     } else {
                         low = mid + 1;
                     }
                 } else {
-                    if (a[mid] < n) {
+                    if (target >= nums[mid] && target <= nums[high]) {
                         low = mid + 1;
                     } else {
                         high = mid - 1;
@@ -40,6 +58,7 @@ public class SesrchRotatedArray {
 
     public static void main(String[] args) {
         SesrchRotatedArray search = new SesrchRotatedArray();
-        System.out.println(search.findBinaryIterative(new int[]{15, 16, 19, 21, 23, 24, 5, 7, 10, 14}, 5));
+        System.out.println(search.findBinaryIterative(new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 8));
+        System.out.println(search.findBinaryRecursive(new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 8, 0, 8));
     }
 }
